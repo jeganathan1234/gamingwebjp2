646661cdfab21533bb829ef20be1cc08eddd5cc1
jeganathan1234 <jega2k02@gmail.com>
2024-10-02 21:23:08 +0530

    commiting the web site

---
+++ game.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById("menu-icon")
+var navlist = document.querySelector(".navlist")
+
+menu.addEventListener("click", function () {
+    navlist.style.right = ('0%')
+})
